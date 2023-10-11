06f2c9eaa3c7e9effb29f7615a3ecfbffd597497
LIAM MORGAN <2203912@uad.ac.uk>
2023-10-11 12:23:43 +0100

    Temp Converter

--- week4/week4/Program.cs
+++ week4/week4/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("This is the main branch");
 Console.WriteLine("Hello");
+
+namespace UtilityKnife.Converters
+{
+    public class FahrenheitToCelsius
+    {
+        public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
+        {
+            double _fahreneithTemp = 0.0;
+            double _celsius = 0.0;
+            return _celsius;
+        }
+    }
+}
